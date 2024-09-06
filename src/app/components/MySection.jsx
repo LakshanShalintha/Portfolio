fef07deabfbd7edfd6c3a2e48a33b344ea31b81b
LakshanShalintha <nlsperera@std.appsc.sab.ac.lk>
2024-09-06 13:08:12 +0530

    Add Animation

--- src/app/components/MySection.jsx
+++ src/app/components/MySection.jsx
@@ -1,31 +1,54 @@
-import React from 'react'
-import Image from 'next/image' 
+"use client"; // Add this line at the top
+
+import React from 'react';
+import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
 
 function MySection() {
   return (
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className="text-white text-4xl sm:text-6xl lg:text-6xl  mb-4 font-extrabold"> 
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#b171d1] to-[#8437f0]">Hello, I'm {" "}
-
+          <h1 className="text-white text-4xl sm:text-6xl lg:text-6xl mb-4 font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#b171d1] to-[#8437f0]">
+              Hello, I'm Lakshan
             </span>
-            Lakshan
           </h1>
-          <p className='text-gray-100 text-base sm:text-lg mb-6 lg:text-xl'>
+          <div className="text-white text-3xl sm:text-4xl lg:text-4xl">
+            <TypeAnimation
+              sequence={[
+                'Web Developer',
+                1000, 
+                'Mobile Developer',
+                1000,
+                '1',
+                1000,
+                '2',
+                1000
+              ]}
+              wrapper="div"
+              speed={50}
+              style={{ fontSize: '1em', display: 'inline-block' }}
+              repeat={Infinity}
+            />
+          </div>
+
+          <p className='text-gray-100 text-base sm:text-lg mb-6 lg:text-xl mt-12'>
             I'm dcide gedbug hbuhde b uedbue c ug uef hdohh eefiueheur urierio ro3
           </p>
-          <dev>
-          <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#a481b6] via-[#8a30b8] to-[#6b03fc] hover:bg-slate-200 text-white'>                
+          <div>
+            <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#a481b6] via-[#8a30b8] to-[#6b03fc] hover:bg-slate-200 text-white'>                
               Contact Me
             </button>
-            <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-[#a481b6] via-[#8a30b8] to-[#6b03fc]  hover:bg-slate-800 text-white mt-3 justify-center'>
-                <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-4 py-2">Downloard CV</span>
+            <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-[#a481b6] via-[#8a30b8] to-[#6b03fc] hover:bg-slate-800 text-white mt-3 justify-center'>
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-4 py-2">
+                Download CV
+              </span>
             </button>
-          </dev>
+          </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-          <div className="rounded-full bg-[#090909]  w-[350px] h-[350px] lg:w[400px] lg:h[400px] relative">
+          <div className="rounded-full bg-[#090909] w-[350px] h-[350px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/Myimages/my-image.png"
               alt="my image"
@@ -40,4 +63,4 @@ function MySection() {
   )
 }
 
-export default MySection
+export default MySection;
