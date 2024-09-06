2b868c14e0cab25c4f86d12492a80eb50e939881
LakshanShalintha <nlsperera@std.appsc.sab.ac.lk>
2024-09-06 12:08:30 +0530

    Change Color

--- src/app/components/MySection.jsx
+++ src/app/components/MySection.jsx
@@ -7,17 +7,20 @@ function MySection() {
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white text-4xl sm:text-6xl lg:text-6xl  mb-4 font-extrabold"> 
-            Hello, I'm Lakshan
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#b171d1] to-[#8437f0]">Hello, I'm {" "}
+
+            </span>
+            Lakshan
           </h1>
-          <p className='text-gray-100 text-base sm:text-lg mb-6 lg:text-xl'>yy
+          <p className='text-gray-100 text-base sm:text-lg mb-6 lg:text-xl'>
             I'm dcide gedbug hbuhde b uedbue c ug uef hdohh eefiueheur urierio ro3
           </p>
           <dev>
           <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#a481b6] via-[#8a30b8] to-[#6b03fc] hover:bg-slate-200 text-white'>                
               Contact Me
             </button>
-            <button className='px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3 justify-center'>
-                Downloard CV
+            <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-[#a481b6] via-[#8a30b8] to-[#6b03fc]  hover:bg-slate-800 text-white mt-3 justify-center'>
+                <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-4 py-2">Downloard CV</span>
             </button>
           </dev>
         </div>
