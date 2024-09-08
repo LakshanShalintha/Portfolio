7af00c9223662a4b0b849639c5e167502507b036
LakshanShalintha <nlsperera@std.appsc.sab.ac.lk>
2024-09-09 01:33:45 +0530

    Create navbar

--- Portfolio/src/app/page.tsx
+++ Portfolio/src/app/page.tsx
@@ -1,9 +1,11 @@
+import NavBar from "./components/common/NavBar";
 import MySection from "./components/MySection";
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black">
+      <NavBar />
         <div className="container mx-auto px-12 py-4">
           <MySection />
         </div>
