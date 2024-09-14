9e37e31a63a5f9877349b1cd8442494f3f20ce1b
LakshanShalintha <nlsperera@std.appsc.sab.ac.lk>
2024-09-14 17:28:33 +0530

    Replace NavBar

--- Portfolio/src/app/page.tsx
+++ Portfolio/src/app/page.tsx
@@ -1,6 +1,9 @@
+import Footer from "./components/common/Footer";
 import NavBar from "./components/common/NavBar";
 import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
 import MySection from "./components/pages/MySection";
+import Projects from "./components/pages/Projects";
 
 
 export default function Home() {
@@ -10,7 +13,10 @@ export default function Home() {
         <div className="container mt-24 mx-auto px-12 py-4">
           <MySection />
           <About />
+          <Projects />
+          <Contact />
         </div>
+      <Footer />  
     </main>
   );
 }
