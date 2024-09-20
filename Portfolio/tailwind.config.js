537f4e4fca6af3279ed76bdb4ad879ff45b9d25c
LakshanShalintha <nlsperera@std.appsc.sab.ac.lk>
2024-09-20 12:09:17 +0530

    Reinstall

---
+++ Portfolio/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // Adjust this path based on your project's structure
+      "./public/index.html", // Include your HTML files as needed
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
