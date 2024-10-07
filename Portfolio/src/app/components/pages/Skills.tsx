58a953d80806b8570d536fc902661371ef3c9b8a
LakshanShalintha <nlsperera@std.appsc.sab.ac.lk>
2024-10-07 16:44:48 +0530

    Create askill page

---
+++ Portfolio/src/app/components/pages/Skills.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+function Skills() {
+  return (
+    <><div>Skills</div><div className="flex justify-center -mt-4 overflow-x-auto">
+          <div className="flex items-center space-x-4 min-w-max">
+              <img
+                  src="/images/Technology/html.jpeg"
+                  alt="HTML"
+                  className="w-16 h-16 object-fill border-2 border-gray-500 rounded-lg p-2 animation-scale-float " />
+              <img
+                  src="/images/Technology/js.png"
+                  alt="JavaScript"
+                  className="w-16 h-16 object-fill border-2 border-gray-500 rounded-lg p-2 animation-scale-float " />
+              <img
+                  src="/images/Technology/tailwind.png"
+                  alt="Tailwind CSS"
+                  className="w-16 h-16 object-fill border-2 border-gray-500 rounded-lg p-2 animation-scale-float " />
+              <img
+                  src="/images/Technology/java.png"
+                  alt="Java"
+                  className="w-16 h-16 object-fill border-2 border-gray-500 rounded-lg p-2 animation-scale-float " />
+              <img
+                  src="/images/Technology/react.png"
+                  alt="React"
+                  className="w-16 h-16 object-fill border-2 border-gray-500 rounded-lg p-2 animation-scale-float " />
+              <img
+                  src="/images/Technology/ts.png"
+                  alt="TypeScript"
+                  className="w-16 h-16 object-fill border-2 border-gray-500 rounded-lg p-2 animation-scale-float " />
+              <img
+                  src="/images/Technology/laravel.png"
+                  alt="Laravel"
+                  className="w-16 h-16 object-fill border-2 border-gray-500 rounded-lg p-2 animation-scale-float " />
+              <img
+                  src="/images/Technology/c.png"
+                  alt="C"
+                  className="w-16 h-16 object-fill border-2 border-gray-500 rounded-lg p-2 animation-scale-float " />
+              <img
+                  src="/images/Technology/dart.png"
+                  alt="Dart"
+                  className="w-16 h-16 object-fill border-2 border-gray-500 rounded-lg p-2 animation-scale-float " />
+              <img
+                  src="/images/Technology/next.png"
+                  alt="Next.js"
+                  className="w-16 h-16 object-fill border-2 border-gray-500 rounded-lg p-2 animation-scale-float " />
+              <img
+                  src="/images/Technology/node-js.png"
+                  alt="Node.js"
+                  className="w-16 h-16 object-fill border-2 border-gray-500 rounded-lg p-2 animation-scale-float " />
+              <img
+                  src="/images/Technology/Firebase.png"
+                  alt="Firebase"
+                  className="w-16 h-16 object-fill border-2 border-gray-500 rounded-lg p-2 animation-scale-float " />
+              <img
+                  src="/images/Technology/mysql.png"
+                  alt="MySQL"
+                  className="w-16 h-16 object-fill border-2 border-gray-500 rounded-lg p-2 animation-scale-float " />
+              <img
+                  src="/images/Technology/github.png"
+                  alt="GitHub"
+                  className="w-16 h-16 object-fill border-2 border-gray-500 rounded-lg p-2 animation-scale-float " />
+              <img
+                  src="/images/Technology/trello.jpeg"
+                  alt="Trello"
+                  className="w-16 h-16 object-fill border-2 border-gray-500 rounded-lg p-2 animation-scale-float " />
+          </div>
+      </div></>
+  )
+}
+
+export default Skills
