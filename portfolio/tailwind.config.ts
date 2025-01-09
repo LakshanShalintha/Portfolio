67fd19b8ed665764f50a1614d9f678a37d103e43
LakshanShalintha <nlsperera@std.appsc.sab.ac.lk>
2025-01-09 17:17:04 +0530

    Add dependency & Dark mood changes

--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -9,10 +9,24 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lightHover: "#efe6e7",
+        darkHover: "#2a004a",
+        darkTheme: "#11001F",
+        lightTheme: "#e6e5e5",
       },
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"], 
+        Ovo: ["Ovo", "serif"],
+      },
+      boxShadow:{
+        'black': '4px 4px 0 #000',
+        'white': '4px 4px 0 #fff',
+      },
+      gridTemplateColumns:{
+        'auto': 'repeat(auto-fit, minimum(200px, 1rf))',
+      }
     },
   },
+  darkMode: 'selector',
   plugins: [],
 } satisfies Config;
